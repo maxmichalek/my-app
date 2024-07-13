6d426f9eccab3b00ef927a3e68f0c62288f5ab84
Max Michalek <max.michalek@maibornwolff.de>
2024-07-13 23:13:51 +0200

    feat: hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,6 @@ export default function Home() {
             By{" "}
                         <Image
                             src="/vercel.svg"
-                            alt="Vercel Logo"
                             className="dark:invert"
                             width={100}
                             height={24}
