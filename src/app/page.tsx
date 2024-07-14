e6061c7591ed095b857f93828c12d249ca522fb5
Max Michalek <max.michalek@maibornwolff.de>
2024-07-14 15:56:39 +0200

    feat: some feat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Page() {
       <h1>Home</h1>
       <h2>{process.env.ENVIRONMENT}</h2>
       <h3>Some Chame!</h3>
+      <h3>Some Chame!</h3>
       <Link href="/about">About</Link>
     </div>
   );
