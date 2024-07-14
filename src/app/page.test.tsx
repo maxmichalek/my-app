58b7ec09241f7f99be422100fafc29a25d853202
Max Michalek <max.michalek@maibornwolff.de>
2024-07-14 10:47:48 +0200

    feat: add set setup

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
-import Page from '../src/app/page';
+import Page from './page';
 
 test('Page', () => {
   render(<Page />);
