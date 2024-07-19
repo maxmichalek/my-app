9b8eaf3d4810f9b5e75e358421ad19933b70938e
Max Michalek <max.michalek@maibornwolff.de>
2024-07-19 15:54:05 +0200

    feat: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div>
-      <h1>V3</h1>
+      <h1>V4</h1>
       {/* <h2>{process.env.ENVIRONMENT}</h2>
       <h3>Some Change!!!!!!!</h3>
       <Link href="/about">About</Link> */}
