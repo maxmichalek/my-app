52b5974f10749629c335d3aca036c42b2e186ea1
Max Michalek <125046535+maxmichalek@users.noreply.github.com>
2024-07-19 13:40:34 +0200

    feat: test (#17)
    Co-authored-by: Max Michalek <max.michalek@maibornwolff.de>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Link from 'next/link';
-
 export default function Page() {
   return (
     <div>
-      <h1>Home</h1>
-      <h2>{process.env.ENVIRONMENT}</h2>
+      <h1>V1</h1>
+      {/* <h2>{process.env.ENVIRONMENT}</h2>
       <h3>Some Change!!!!!!!</h3>
-      <Link href="/about">About</Link>
+      <Link href="/about">About</Link> */}
     </div>
   );
 }
