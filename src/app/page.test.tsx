b013c304126368daf0141857f70ea730c4032aa7
Max Michalek <max.michalek@maibornwolff.de>
2024-07-20 09:25:22 +0200

    feat: test

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -5,6 +5,6 @@ import Page from './page';
 test('Page', () => {
   render(<Page />);
   expect(
-    screen.getByRole('heading', { level: 1, name: 'DEPLOY' })
+    screen.getByRole('heading', { level: 1, name: 'DEPLOY1' })
   ).toBeDefined();
 });
